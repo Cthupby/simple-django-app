12e61ffb9c015b71163b27360eaa38acb647dd55
Cthupby <Cthupby@gmail.com>
2021-12-06 23:12:09 +0300

    Adding pagination

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import View
 from django.db.models import Q
+from django.core.paginator import Paginator
 from .utils import *
 from .models import Post, Tag
 from .forms import TagForm, PostForm
@@ -14,7 +15,28 @@ def posts_list(request):
     else:
         posts = Post.objects.all()
 
-    return render(request, 'blog/index.html', context={'posts': posts})
+    paginator = Paginator(posts, 2)
+    page_number = request.GET.get('page', 1)
+    page = paginator.get_page(page_number)
+
+    is_paginated = page.has_other_pages()
+    if page.has_previous():
+        prev_url = '?page={}'.format(page.previous_page_number())
+    else:
+        prev_url = ''
+    if page.has_next():
+        next_url = '?page={}'.format(page.next_page_number())
+    else:
+        next_url = ''
+
+    context = {
+        'page_object': page,
+        'is_paginated': is_paginated,
+        'next_url': next_url,
+        'prev_url': prev_url
+    }
+
+    return render(request, 'blog/index.html', context=context)
 
 class PostCreate(ObjectCreateMixin, View):
     model_form = PostForm
