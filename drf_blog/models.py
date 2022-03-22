4defb87b87e31f3c8107644f03deb2beb04feaa4
Cthupby <Cthupby@gmail.com>
2022-03-22 22:47:04 +0300

    Update drf_blog models

--- drf_blog/models.py
+++ drf_blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    url = models.URLField()
+    poster = models.ForeignKey(User, on_delete=models.CASCADE)
+    create = models.DateTimeField(auto_now_add=True)
+        
+    class Mete:
+        ordering = ['-created']
+        
+class Vote(models.Model):
+    voter = models.ForeignKey(User, on_delete=models.CASCADE)
+    poster = models.ForeignKey(Post, on_delete=models.CASCADE)
+
--- simpleapp/settings.py
+++ simpleapp/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'snippets',
     'blog',
     'drf_blog',
     'spa_blog',
