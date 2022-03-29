689bab096b9e45895e69d89ccb8b7598c21ab4ab
Cthupby <Cthupby@gmail.com>
2022-03-29 23:21:49 +0300

    Add api-auth

--- drf_blog/urls.py
+++ drf_blog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import include, path
-from rest_framework import routers
+from rest_framework import routers, include
 from drf_blog import views
 
 
 urlpatterns = [
     path('api/posts', views.PostList.as_view()),
-    path('api/posts/<1nt:pk>/vote', views.VoteCreate.as_view())    
+    path('api/posts/<1nt:pk>/vote', views.VoteCreate.as_view()),
+    path('api-auth/', include('rest_framework.urls'))    
 ]
 
