1dcfe37fd375add1a6d48e2324be7b0902562f76
Cthupby <Cthupby@gmail.com>
2022-03-29 23:16:52 +0300

    Update VoteSerializer

--- drf_blog/views.py
+++ drf_blog/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics, permissions
+from rest_framework.exceptions import ValidationError
 from .serializers import PostSerializer, VoteSerializer
 from .models import Post, Vote
 
@@ -21,10 +22,12 @@ class VoteCreate(generics.CreateAPIView):
     serializer_class = VoteSerializer
     permission_classes = [permissions.IsAuthenticated]
 
-    def get_qureryset(self):
+    def get_queryset(self):
         user = user.request.user
         post = Post.objects.get(pk=self.kwargs['pk'])
         return Vote.objects.filter(voter=user, post=post)
         
     def perform_create(self, serializer):
+        if self.get_queryset().exists():
+            raise ValidationError('You have already voted for this post.')
         serializer.save(voter=self.request.user, post=Post.objects.get(pk=self.kwargs['pk']))
